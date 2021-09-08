60b6eac039fe1b8c2ef1160d9be4e475e919df5b
caoweijian <caoweijian@kedacom.com>
2021-09-08 14:47:15 +0800

    使用viewbinding优化视图

--- app/src/main/java/com/caowj/imageloader/ImageActivity.kt
+++ app/src/main/java/com/caowj/imageloader/ImageActivity.kt
@@ -3,22 +3,21 @@ package com.caowj.imageloader
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.VolleyError
 import com.android.volley.toolbox.ImageLoader
 import com.caowj.imageloader.cache.ImageCacheManager
+import com.caowj.imageloader.databinding.ActivityImageBinding
 
 class ImageActivity : AppCompatActivity() {
-    private lateinit var imageView: ImageView
-    private lateinit var progressLy: View
+    private lateinit var binding: ActivityImageBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_image)
+        binding = ActivityImageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         title = "三级缓存"
-        imageView = findViewById(R.id.iv)
-        progressLy = findViewById(R.id.progress_ly)
+
 
 //        高清壁纸：https://api.ixiaowai.cn/gqapi/gqapi.php
 //        https://tva2.sinaimg.cn/large/0060lm7Tly1ftg6omusg9j31hc0u010h.jpg
@@ -35,15 +34,15 @@ class ImageActivity : AppCompatActivity() {
             object : ImageLoader.ImageListener {
                 override fun onResponse(imageContainer: ImageLoader.ImageContainer, b: Boolean) {
                     Log.d("caowj", "onResponse")
-                    progressLy.visibility = View.GONE
+                    binding.progressLy.visibility = View.GONE
                     if (imageContainer.bitmap != null) {
-                        imageView.setImageBitmap(imageContainer.bitmap)
+                        binding.iv.setImageBitmap(imageContainer.bitmap)
                     }
                 }
 
                 override fun onErrorResponse(volleyError: VolleyError?) {
                     Log.d("caowj", "onErrorResponse")
-                    progressLy.visibility = View.GONE
+                    binding.progressLy.visibility = View.GONE
                 }
             })
     }
@@ -52,9 +51,9 @@ class ImageActivity : AppCompatActivity() {
         //方法二：
         com.caowj.imageloader.cache2.ImageLoader.getInstance(this).display(
             "https://tva2.sinaimg.cn/large/0060lm7Tly1ftg6omusg9j31hc0u010h.jpg",
-            imageView
+            binding.iv
         )
-        progressLy.visibility = View.GONE
+        binding.progressLy.visibility = View.GONE
     }
 
     override fun onDestroy() {
